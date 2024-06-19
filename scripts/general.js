05b35f80c3e05c85675fa05d0a2ce7ae3e7a0796
Vatsapon Asawakittiporn <vatsapon33@gmail.com>
2024-06-19 22:32:51 +0700

    Make Header only response when scroll to certain point.
    - Parallax background (WIP).
    - Scalable footer link icon.

--- scripts/general.js
+++ scripts/general.js
@@ -1,11 +1,50 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("main-header").style.top = "0";
-  } else {
-    document.getElementById("main-header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+
+// Determine to use Desktop/Mobile layout.
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	loadMobile();
+} else {
+	loadDesktop();
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+
+var prevScrollpos = window.pageYOffset;
+let topVal = 0;
+const parallaxForce = 1;
+
+window.onscroll = function() {
+	const currentScrollPos = window.pageYOffset;
+	let headerElement = document.getElementById("main-header");
+	let introElement = document.getElementById("intro-section-video");
+	
+	// Scroll up.
+	if (prevScrollpos > currentScrollPos) {
+		topVal += parallaxForce;
+		//introElement.style.top = topVal + "px";
+		
+		if (currentScrollPos <= 150) {
+			headerElement.style.top = "0px";
+		}
+	}
+	
+	// Scroll down.
+	if (prevScrollpos < currentScrollPos) {
+		topVal -= parallaxForce;
+		//introElement.style.top = topVal + "px";
+		
+		if (currentScrollPos > 150) {
+			headerElement.style.top = "-150px";
+		}
+	}
+	
+	prevScrollpos = currentScrollPos;
+}
+
+function loadDesktop() {
+	
+}
+
+function loadMobile() {
+	
+}
+
