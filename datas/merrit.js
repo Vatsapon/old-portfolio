e5a92671cbb42df837d4377b712130e8fd881352
Vatsapon <vatsapon33@gmail.com>
2024-06-30 11:38:12 +0700

    Added Gallery Preview mode, Rework on Database system (WIP)

---
+++ datas/merrit.js
@@ -0,0 +1,38 @@
+const galleryData = [{
+	id: 'gallery-gameplay',
+	gallery: [{
+		source: 'images/merrit/merrit-screenshot-1.png',
+		name: 'Merrit Screenshot 1',
+		description: '',
+	}, {
+		source: 'images/merrit/merrit-screenshot-2.png',
+		name: 'Merrit Screenshot 2',
+		description: '',
+	}, {
+		source: 'images/merrit/merrit-screenshot-3.png',
+		name: 'Merrit Screenshot 3',
+		description: '',
+	}],
+}, {
+	id: 'gallery-award',
+	gallery: [{
+		source: 'images/merrit/merrit-award.jpg',
+		name: 'Game Talent Showcase Awards',
+		description: 'Our team representation received the Talent of the Year 2022 Award',
+	}, {
+		source: 'images/merrit/merrit-certificate.png',
+		name: 'Talent of the Year 2022 Certificated',
+		description: '',
+	}],
+}, {
+	id: 'gallery-script',
+	gallery: [{
+		source: 'images/merrit/merrit-player-statm.png',
+		name: 'Player Stats System',
+		description: 'This will display the player/'s stat',
+	}, {
+		source: 'images/merrit/merrit-combatm.png',
+		name: 'Talent of the Year 2022 Certificated',
+		description: '',
+	}],
+}];
--- scripts/project.js
+++ scripts/project.js
@@ -15,6 +15,15 @@ function setUp() {
 			id: galleryID,
 			index: 0,
 		}
+		
+		// Add Preview button event.
+		let galleryContents = galleries[i].getElementsByClassName('gallery-content');
+		
+		for (let j = 0; j < galleryContents.length; j++) {
+			galleryContents[j].addEventListener('click', () => {
+				setGalleryPreview(galleryID, j);
+			});
+		}
 	}
 	
 	setInterval(() => {
@@ -26,21 +35,15 @@ function setUp() {
 
 function setSlide(elementID, index) {
 	const galleryData = getGalleryFromID(elementID);
+	
 	if (!galleryData) return;
 	
 	const galleryElement = document.getElementById(elementID).getElementsByClassName("gallery-content");
 
-	// Index less, loop back to element's length.
-	if (galleryData.index < 0) {
-		galleryData.index = galleryElement.length - 1;
-	}
+	if (galleryData.index < 0) galleryData.index = galleryElement.length - 1;
 
-	// Index exceed length, loop back to first.
-	if (galleryData.index > galleryElement.length - 1) {
-		galleryData.index = 0;
-	}
+	if (galleryData.index > galleryElement.length - 1) galleryData.index = 0;
 	
-	// Set all display to none, except selected index.
 	for (let i = 0; i < galleryElement.length; i++) {
 		galleryElement[i].style.display = i=== galleryData.index ? 'block' : 'none';
 	}
@@ -70,19 +73,102 @@ function getGalleryFromID(galleryID) {
 	return null;
 }
 
-function closeIMGPreview() {
-	const imgPreviewElement = document.getElementById('img-preview');
-	imgPreviewElement.style.display = 'none';
+var previewDatas = [];
+var previewIndex = 0;
+
+// Image Preview.
+
+function nextPreview() {
+	previewIndex++;
+	showPreview();
 }
 
-function openIMGPreview(source, name, description) {
+function prevPreview() {
+	previewIndex--;
+	showPreview();
+}
+
+function showPreview() {
+	
+	if (previewIndex < 0) previewIndex = previewDatas.length - 1;
+	
+	if (previewIndex > previewDatas.length - 1) previewIndex = 0;
+	
+	const previewData = previewDatas[previewIndex];
+	
 	const imgPreviewElement = document.getElementById('img-preview');
 	const imgDisplayElement = document.getElementById('img-preview-display');
 	const imgNameElement = document.getElementById('img-preview-name');
 	const imgDescriptionElement = document.getElementById('img-preview-desc');
 	
-	imgDisplayElement.src = source;
+	imgDisplayElement.src = previewData.source;
 	imgPreviewElement.style.display = 'block';
-	imgNameElement.innerHTML = name;
-	imgDescriptionElement.innerHTML = description;
+	imgNameElement.innerHTML = previewData.name;
+	imgDescriptionElement.innerHTML = previewData.description;
+}
+
+function closePreview() {
+	previewDatas = [];
+	previewIndex = 0;
+	
+	const imgPreviewElement = document.getElementById('img-preview');
+	imgPreviewElement.style.display = 'none';
+}
+
+function setIMGPreview(source, name, description) {
+	
+	// If there's image previewing, cancel.
+	if (previewDatas.length > 0) return;
+	
+	previewIndex = 0;
+	
+	let imgPreview = {
+		source: source,
+		name: name,
+		description: description,
+	}
+	
+	const imgPreviewElement = document.getElementById('img-preview');
+	const prevButtonElement = imgPreviewElement.getElementsByClassName('prev-btn');
+	const nextButtonElement = imgPreviewElement.getElementsByClassName('next-btn');
+	
+	prevButtonElement[0].style.display = 'none';
+	nextButtonElement[0].style.display = 'none';
+	
+	previewDatas[0] = imgPreview;
+	showPreview();
+}
+
+function setGalleryPreview(galleryID, index) {
+	// If there's image previewing, cancel.
+	if (previewDatas.length > 0) return;
+	
+	const galleryData = getGalleryFromID(galleryID);
+	
+	if (!galleryData) return;
+	
+	const galleryElement = document.getElementById(galleryID).getElementsByClassName('gallery-content');
+
+	for (var i = 0; i < galleryElement.length; i++) {
+		var imgData = galleryElement[i].getElementsByTagName('img')[0];
+		
+		previewDatas[i] = {
+			source: imgData.src,
+			name: imgData.alt,
+			description: '',
+		}
+	}
+
+	previewIndex = index;
+	
+	if (galleryElement.length > 1) {
+		const imgPreviewElement = document.getElementById('img-preview');
+		const prevButtonElement = imgPreviewElement.getElementsByClassName('prev-btn');
+		const nextButtonElement = imgPreviewElement.getElementsByClassName('next-btn');
+		
+		prevButtonElement[0].style.display = 'block';
+		nextButtonElement[0].style.display = 'block';
+	}
+	
+	showPreview();
 }
